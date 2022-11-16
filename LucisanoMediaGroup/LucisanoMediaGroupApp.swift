bcdf40fd59740971f91e53016fef038115b6f74b
valikleha <57154611+valikleha@users.noreply.github.com>
2022-11-16 16:38:38 +0100

    Initial Commit

---
+++ LucisanoMediaGroup/LucisanoMediaGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucisanoMediaGroupApp.swift
+//  LucisanoMediaGroup
+//
+//  Created by Alexey on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LucisanoMediaGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
